24c658dcb75540cdd773e90633433e2503c80a03
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-07-28 14:16:07 +0100

    feat: adding sliding effect

--- js/main.js
+++ js/main.js
@@ -1,27 +1,81 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const timeline = document.querySelector(".timeline");
+
   const timelineContentTop = document.querySelector(
     ".timeline-history-content-top",
   );
-
   const timelineContentBottom = document.querySelector(
     ".timeline-history-content-bottom",
   );
 
+  const leftControl = document.querySelector(".timeline-controls-left");
+  const rightControl = document.querySelector(".timeline-controls-right");
+
+  // get timeline width
+  const timelineWidth = timeline.offsetWidth;
+  const timelineContentCardWidth = 384;
+
+  let cardIndex = 0;
+
+  // set timeline content width
   const setTimelineContentWidth = (content) => {
     const timelineContentCards = content.querySelectorAll(".timeline-card");
     const timelineContentCardsLength = timelineContentCards.length;
-    const timelineContentCardWidth = 288;
-    const timelineContentGap = 64;
-    const timelineContentWidth =
-      timelineContentCardsLength * timelineContentCardWidth +
-      timelineContentGap * (timelineContentCardsLength - 1);
 
+    const timelineContentWidth =
+      timelineContentCardsLength * timelineContentCardWidth;
     content.style.width = `${timelineContentWidth}px`;
   };
 
+  // init timeline
   const initTimeline = () => {
     setTimelineContentWidth(timelineContentTop);
     setTimelineContentWidth(timelineContentBottom);
+
+    let timelineContentTopOffsetLeft = timelineContentTop.offsetLeft;
+    let timelineContentBottomOffsetLeft = timelineContentBottom.offsetLeft;
+
+    let targetLeft = 0;
+    let animationFrameId = null;
+
+    const animateSlide = () => {
+      if (timelineContentTopOffsetLeft !== targetLeft) {
+        timelineContentTopOffsetLeft +=
+          (targetLeft - timelineContentTopOffsetLeft) * 0.1;
+        timelineContentBottomOffsetLeft +=
+          (targetLeft - timelineContentBottomOffsetLeft) * 0.1;
+
+        timelineContentTop.style.left = `${timelineContentTopOffsetLeft}px`;
+        timelineContentBottom.style.left = `${timelineContentBottomOffsetLeft}px`;
+
+        animationFrameId = requestAnimationFrame(animateSlide);
+      }
+    };
+
+    leftControl.addEventListener("click", () => {
+      if (Math.round(timelineContentTopOffsetLeft) < 0) {
+        targetLeft += timelineWidth / 6;
+        if (targetLeft > 0) targetLeft = 0;
+
+        if (!animationFrameId) {
+          animationFrameId = requestAnimationFrame(animateSlide);
+        }
+        if (cardIndex > 0) cardIndex--;
+      }
+    });
+
+    rightControl.addEventListener("click", () => {
+      if (timelineContentTopOffsetLeft >= -timelineWidth) {
+        targetLeft -= timelineWidth / 6;
+        if (targetLeft < -timelineWidth - timelineContentCardWidth / 2)
+          targetLeft = -timelineWidth - timelineContentCardWidth / 2;
+        if (!animationFrameId) {
+          animationFrameId = requestAnimationFrame(animateSlide);
+        }
+        cardIndex++;
+        console.log(cardIndex);
+      }
+    });
   };
 
   initTimeline();
